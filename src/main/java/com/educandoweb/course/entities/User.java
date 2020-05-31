aebc867dbd82ce566280f1b2fe9a45009828908e
Anderson Monteiro <anderson.a.monteiro@outlook.com.br>
2020-05-31 18:44:25 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -2,7 +2,15 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//Auto incremento
 	private Integer id;
 	private String name;
 	private String email;
